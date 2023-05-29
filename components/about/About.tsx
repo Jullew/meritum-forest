bb65bd52807acf8b90fc034ae286be7f405a2ae8
Julian <jullewdevpl@gmail.com>
2023-05-29 22:48:30 +0200

    icons off for tests

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
-import {
-  GiPulleyHook,
-  GiGrass,
-  GiCircularSaw,
-  GiHighGrass,
-  GiHighTide,
-  GiFruitTree,
-} from "react-icons/gi";
-import HiArrowLongRight from "react-icons/hi2";
+// import {
+//   GiPulleyHook,
+//   GiGrass,
+//   GiCircularSaw,
+//   GiHighGrass,
+//   GiHighTide,
+//   GiFruitTree,
+// } from "react-icons/gi";
+// import { HiArrowLongRight } from "react-icons/hi2";
 import { motion } from "framer-motion";
 import { fadeIn, slideIn } from "@/lib/motion";
 
@@ -26,7 +26,7 @@ function About({}: Props) {
         </h2>
         <div className="mt-10 flex flex-wrap">
           <div className="w-1/3 border p-4 group relative hover:bg-green-900 hover:text-white ease-linear transition-all border-gray-400 flex flex-col">
-            <GiPulleyHook size={50} />
+            {/* <GiPulleyHook size={50} /> */}
             <h2 className="text-xl font-medium h-[10%]">
               Specjalistyczne usługi alpinistyczne (arborystyka)
             </h2>
@@ -36,11 +36,11 @@ function About({}: Props) {
               linii energetycznych itp.
             </p>
             <div className="ease-linear absolute right-4 bottom-4 transition-all group-hover:translate-x-2">
-              <HiArrowLongRight size={35} />
+              {/* <HiArrowLongRight size={35} /> */}
             </div>
           </div>
           <div className="w-1/3 border p-4 group  relative hover:bg-green-900 hover:text-white ease-linear transition-all border-gray-400 flex flex-col">
-            <GiHighGrass size={50} />
+            {/* <GiHighGrass size={50} /> */}
             <h2 className="text-xl font-medium h-[10%]">Koszenie trawników</h2>
             <p>
               Jeśli marzy nam się piękny zadbany trawnik musimy w okresie od
@@ -48,11 +48,11 @@ function About({}: Props) {
               Zabieg tan sprawi, że nasza trawa będzie zadbana, gęsta i zdrowa.
             </p>
             <div className="ease-linear absolute right-4 bottom-4 transition-all group-hover:translate-x-2">
-              <HiArrowLongRight size={35} />
+              {/* <HiArrowLongRight size={35} /> */}
             </div>
           </div>
           <div className="w-1/3 border p-4 relative group hover:bg-green-900 hover:text-white ease-linear transition-all border-gray-400 flex flex-col">
-            <GiHighTide size={50} />
+            {/* <GiHighTide size={50} /> */}
             <h2 className="text-xl font-medium h-[10%]">
               Wycinanie drzew pod liniami energetycznymi
             </h2>
@@ -61,13 +61,13 @@ function About({}: Props) {
               dla naszej firmy główny nurt działań.
             </p>
             <div className="ease-linear absolute right-4 bottom-4 transition-all group-hover:translate-x-2">
-              <HiArrowLongRight size={35} />
+              {/* <HiArrowLongRight size={35} /> */}
             </div>
           </div>
         </div>
         <div className="flex flex-wrap mt-0">
           <div className="w-1/3 border p-4 group relative hover:bg-green-900 hover:text-white ease-linear transition-all border-gray-400 flex flex-col">
-            <GiGrass size={50} />
+            {/* <GiGrass size={50} /> */}
             <h2 className="text-xl font-medium h-[10%]">
               Strzyżenie żywopłotów
             </h2>
@@ -76,29 +76,29 @@ function About({}: Props) {
               temu zabiegowi będzie on zdrowy i gęsty od góry do samego dołu.
             </p>
             <div className="ease-linear absolute right-4 bottom-4 transition-all group-hover:translate-x-2">
-              <HiArrowLongRight size={35} />
+              {/* <HiArrowLongRight size={35} /> */}
             </div>
           </div>
           <div className="w-1/3 border p-4 group relative hover:bg-green-900 hover:text-white ease-linear transition-all border-gray-400 flex flex-col">
-            <GiCircularSaw size={50} />
+            {/* <GiCircularSaw size={50} /> */}
             <h2 className="text-xl font-medium h-[10%]">Wycinanie drzew</h2>
             <p>
               Nasza firma wykonuje każdą usługę związaną z wycięciem lub
               przycięciem drzewa.
             </p>
             <div className="ease-linear absolute right-4 bottom-4 transition-all group-hover:translate-x-2">
-              <HiArrowLongRight size={35} />
+              {/* <HiArrowLongRight size={35} /> */}
             </div>
           </div>
           <div className="w-1/3 border p-4 relative group hover:bg-green-900 hover:text-white ease-linear transition-all border-gray-400 flex flex-col">
-            <GiFruitTree size={50} />
+            {/* <GiFruitTree size={50} /> */}
             <h2 className="text-xl font-medium h-[10%]">Pielęgnacja drzew</h2>
             <p>
               Pielęgnacja drzew i krzewów polega na cięciu konarów i gałęzi, tak
               by stymulować ich prawidłowy wzrost.
             </p>
             <div className="ease-linear absolute right-4 bottom-4 transition-all group-hover:translate-x-2">
-              <HiArrowLongRight size={35} />
+              {/* <HiArrowLongRight size={35} /> */}
             </div>
           </div>
         </div>
