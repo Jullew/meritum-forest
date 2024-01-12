2cc951181a2889815ce5d6f0db3ce782e403fcfc
Julian Lewandowski <jullewdevpl@gmail.com>
2024-01-12 13:31:12 +0100

    Vercel's Speed Insights added

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -3,6 +3,8 @@ import "../globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/layout/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +26,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
